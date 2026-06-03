7c0d09a2369c3d9f411f8529176b513a0182168d
Trebes Stan <stan.trebes@etu.unistra.fr>
2026-06-03 15:11:02 +0200

    feat: lien Mon compte vers la page connexion

--- resources/views/components/app-header.blade.php
+++ resources/views/components/app-header.blade.php
@@ -8,12 +8,12 @@
             J'ai un code
         </button>
 
-        <button class="btn-account" type="button" aria-label="Mon compte">
+        <a href="{{ route('login') }}" class="btn-account" aria-label="Mon compte">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <circle cx="12" cy="8" r="4"/>
                 <path d="M4 20c0-4 3.6-7 8-7s8 3 8 7"/>
             </svg>
             <span>Mon compte</span>
-        </button>
+        </a>
     </div>
 </header>
