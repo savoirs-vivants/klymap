284eceb76a7e4bd0239879c71de816537aa36b3a
Trebes Stan <stan.trebes@etu.unistra.fr>
2026-06-03 15:11:25 +0200

    feat: routes auth en français avec middlewares guest et auth

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\ResetPasswordController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', fn () => view('welcome'))->name('home');
+
+Route::middleware('guest')->group(function () {
+    Route::get('/inscription', [RegisterController::class, 'show'])->name('register');
+    Route::post('/inscription', [RegisterController::class, 'store']);
+
+    Route::get('/connexion', [LoginController::class, 'show'])->name('login');
+    Route::post('/connexion', [LoginController::class, 'store']);
+
+    Route::get('/mot-de-passe-oublie', [ForgotPasswordController::class, 'show'])->name('password.request');
+    Route::post('/mot-de-passe-oublie', [ForgotPasswordController::class, 'store'])->name('password.email');
+
+    Route::get('/reinitialiser/{token}', [ResetPasswordController::class, 'show'])->name('password.reset');
+    Route::post('/reinitialiser', [ResetPasswordController::class, 'store'])->name('password.update');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::post('/deconnexion', [LoginController::class, 'destroy'])->name('logout');
 });
