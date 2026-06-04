41fdb925cd69db84044d93754ce8a6003855a0b0
Trebes Stan <stan.trebes@etu.unistra.fr>
2026-06-04 11:44:26 +0200

    Modification de la route de logout

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -37,6 +37,6 @@ class LoginController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return redirect()->route('login');
+        return redirect()->route('home');
     }
 }
