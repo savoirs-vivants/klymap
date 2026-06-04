74d58b62a1f5706351f0559428c0c3a2d656322e
Trebes Stan <stan.trebes@etu.unistra.fr>
2026-06-04 11:24:33 +0200

    Mise en place des routes

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,11 @@ use App\Http\Controllers\CapteurTemoinController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\CampagneController;
+use App\Http\Controllers\ParticipantController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => view('welcome'))->name('home');
+Route::get('/', fn() => view('welcome'))->name('home');
 
 Route::middleware('guest')->group(function () {
     Route::get('/inscription', [RegisterController::class, 'show'])->name('register');
@@ -22,10 +24,15 @@ Route::middleware('guest')->group(function () {
 
     Route::get('/reinitialiser/{token}', [ResetPasswordController::class, 'show'])->name('password.reset');
     Route::post('/reinitialiser', [ResetPasswordController::class, 'store'])->name('password.update');
+
+    Route::get('/code',               [ParticipantController::class, 'showJoin'])->name('participant.join');
+    Route::post('/code/valider',      [ParticipantController::class, 'validateCode'])->name('participant.validateCode');
+    Route::post('/session/rejoindre', [ParticipantController::class, 'register'])->name('participant.register');
+    Route::post('/session/quitter',   [ParticipantController::class, 'logout'])->name('participant.logout');
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', fn () => view('welcome'))->name('dashboard');
+    Route::get('/dashboard', fn() => view('welcome'))->name('dashboard');
     Route::post('/deconnexion', [LoginController::class, 'destroy'])->name('logout');
 
     Route::prefix('api')->name('api.')->group(function () {
@@ -35,6 +42,13 @@ Route::middleware('auth')->group(function () {
         Route::delete('/capteur-temoins/{capteurTemoin}', [CapteurTemoinController::class, 'destroy'])->name('temoins.destroy');
         Route::get('/capteur-temoins/{capteurTemoin}/export', [CapteurTemoinController::class, 'export'])->name('temoins.export');
     });
+
+    Route::get('/campagnes',                      [CampagneController::class, 'index'])->name('campagnes.index');
+    Route::post('/campagne',                      [CampagneController::class, 'store'])->name('campagne.store');
+    Route::put('/campagnes/{campagne}',           [CampagneController::class, 'update'])->name('campagne.update');
+    Route::get('/campagnes/{campagne}/participants', [CampagneController::class, 'participants'])->name('campagne.participants');
+    Route::put('/campagnes/{campagne}/terminer', [CampagneController::class, 'terminer'])->name('campagnes.terminer');
+    Route::delete('/campagnes/{campagne}',        [CampagneController::class, 'destroy'])->name('campagne.destroy');
 });
 
 Route::middleware(['auth', 'admin'])->prefix('backoffice')->name('backoffice.')->group(function () {
