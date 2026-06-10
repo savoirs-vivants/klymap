ea7ba1bf6e8119b33c5baf59aeb2c7088fe9e8a7
Trebes Stan <stan.trebes@etu.unistra.fr>
2026-06-10 13:28:26 +0200

    fix: erreur 500 sur /code en prod - entrée Vite inexistante

--- resources/views/participant/join.blade.php
+++ resources/views/participant/join.blade.php
@@ -11,7 +11,7 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&family=Space+Mono:wght@400;700&display=swap"
         rel="stylesheet">
-    @vite(['resources/css/app.css', 'resources/js/join-session.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="bg-slate-50 font-grotesk text-slate-900 antialiased min-h-screen flex flex-col">
